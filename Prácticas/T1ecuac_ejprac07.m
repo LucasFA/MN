5d805bdc33676d1dfdf73fd0eef65b8409bb3627
LucasFA <lucaslanzarote@gmail.com>
2022-03-25 00:58:35 +0100

    Terminado ejercicio 7

--- Prácticas/T1ecuac_ejprac07.m
+++ Prácticas/T1ecuac_ejprac07.m
@@ -1,20 +1,23 @@
 clc; clear all;
 
 function y = f(x)
-  y = [4*x(1)^2+x(2)^2-4; x(1) + x(2) - sin(x(1)-x(2))];
+    y = [4 * x(1)^2 + x(2)^2 - 4; x(1) + x(2) - sin(x(1) - x(2))];
 endfunction
-
+% El jacobiano de la f definida aquí ^ encima
 function M = J(x)
-  M = [8*x(1), 2*x(2); 1- cos(x(1)-x(2), 1 - cos(x(1)-x(2)];
+    M = [8 * x(1), 2 * x(2); 1 - cos(x(1) - x(2)), 1 + cos(x(1) - x(2))];
 endfunction
 
-tol = 10^(-8)
-x1 = x0 = 0;
+% Inicializamos
+nmax = 100;
+tol = 10^(-8);
+x0 = x1 = [0.5; 0.5];
 n = 0;
+% Ejecutamos NR
 do
-  x0 = x1;
-  x1 = x0 - J(x0)\f(x0);
-  n++;
-until norm(x1-x0, 1) < tol
-
-printf("Se obtiene en ")
+    x0 = x1;
+    x1 = x0 - J(x0)\f(x0);
+    n++;
+until (norm(x1 - x0, 1) < tol || n > nmax)
+x1
+printf("Se obtiene la solucion aproximada (%0.6f,%0.6f) tras %i iteraciones\n", x1(1), x1(2), n)
