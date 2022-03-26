5da06072bb4a383b1c191a02c13c80b0790a367b
LucasFA <lucaslanzarote@gmail.com>
2022-03-26 15:53:15 +0100

    Tiny lint

--- Practicas/T1ecuac_ejprac05.m
+++ Practicas/T1ecuac_ejprac05.m
@@ -10,12 +10,12 @@ function y = deriv_f(x)
     y = 3 * x.^2 + 9;
 endfunction
 
-
 printf("Aplicaremos Newton Raphson a la funcion f del ejercicio 2\n\n")
 
 printf("Semilla      Aproximacion     Iteraciones       Mensaje\n");
 
 semillas = linspace(-3, 0, 11);
+
 for s = semillas
     [aprox, n, msg] = fNewtonRaphson(@f, @deriv_f, s, 10^(-8), 100);
     printf("% 4.2f %21.14f %8i    %30s \n", s, aprox, n, msg);
