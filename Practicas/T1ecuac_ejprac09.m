8d4151cfba17ca198d46bad083523890b7cd3c36
LucasFA <lucaslanzarote@gmail.com>
2022-04-01 12:30:37 +0200

    Añadido comentarios y arreglado nº iteraciones

--- Practicas/T1ecuac_ejprac09.m
+++ Practicas/T1ecuac_ejprac09.m
@@ -16,22 +16,21 @@ function x1 = NR2(x0)
     fu0 = f(x0);
     fu1 = f1(x0);
     fu2 = f2(x0);
-
-    discr = sqrt(fu1^2 - 2 * fu0 * fu2);
-    h1 = (-fu1 + discr) / fu2; # raices h = x1 - x0
+    # p(x) = ax^2 + bx + c = fu2 / 2 * x^2 + f1 * x + f0 la parábola que interpola f(x0) f'(x0) y f''(x0)
+    discr = sqrt(fu1^2 - 2 * fu0 * fu2); # bueno, el discriminante sería lo de dentro de la raíz
+    h1 = (-fu1 + discr) / fu2; # x + h1, x + h2 son las raíces de la parábola
     h2 = (-fu1 - discr) / fu2;
-    if (abs(h1) < abs(h2))
-        y = x0 + h1;
+    if (abs(h1) < abs(h2)) # tomamos la raíz más cercana a x0
+        x1 = x0 + h1;
     else
-        y = x0 + h2;
+        x1 = x0 + h2;
     endif
-    x1 = y;
 endfunction
 
 numero_iteraciones = 6;
 x0 = x1 = 1.5;
 disp("n        NR2");
-for n = 1:numero_iteraciones
+for n = 0:numero_iteraciones
     printf("%i  %16.12f\n", n, x1);
     x1 = NR2(x0);
     x0 = x1;
