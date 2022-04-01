0ffb8dca1c37fcd94940cc622e6f85408aec5a00
LucasFA <lucaslanzarote@gmail.com>
2022-04-01 02:00:35 +0200

    Añadir el código de NR2
    Para hacer la comparativa

--- Practicas/T1ecuac_ejprac08.m
+++ Practicas/T1ecuac_ejprac08.m
@@ -4,6 +4,31 @@ function y = f(x)
     y = 1 - x +0.3 * cos(x);
 endfunction
 
+function y = f1(x)
+    y = -1 -0.3 * sin(x);
+endfunction
+
+function y = f2(x)
+    y = -0.3 * cos(x);
+endfunction
+
+function x1 = NR2(x0, f, f1, f2)
+    x1 = 0;
+    fu0 = f(x0);
+    fu1 = f1(x0);
+    fu2 = f2(x0);
+
+    discr = sqrt(fu1^2 - 2 * fu0 * fu2);
+    h1 = (-fu1 + discr) / fu2; # raices h = x1 - x0
+    h2 = (-fu1 - discr) / fu2;
+    if (abs(h1) < abs(h2))
+        y = x0 + h1;
+    else
+        y = x0 + h2;
+    endif
+    x1 = y;
+endfunction
+
 function x3 = Muller(f, x0, x1, x2)
     f0 = f(x0);
     f1 = f(x1);
