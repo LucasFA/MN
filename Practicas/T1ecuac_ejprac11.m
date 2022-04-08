51d93449c78dbc8d389846d6b5136c381344ab99
LucasFA <lucaslanzarote@gmail.com>
2022-04-08 09:05:57 +0200

    Ejercicio básico
    Falta añadir los plots que había hecho

--- Practicas/T1ecuac_ejprac11.m
+++ Practicas/T1ecuac_ejprac11.m
@@ -2,20 +2,35 @@ clc; clear all; more off;
 function y = f(x)
     y(1) = sin(x(1) * x(2)) / 2 - x(2) / (4 * pi) - x(1) / 2;
     y(2) = (1 - 1 / (4 * pi)) * (exp(2 * x(1)) - e) + (e / pi) * x(2) - 2 * e * x(1);
+    y = y';
 endfunction
-
-s1 = [-2, -2];
-s2 = [-1, -1];
-s3 = [0, 0];
-s4 = [1, 1];
-s5 = [2, 2];
 
 tol = 10^(-8);
-
-function y = paso_NR(semilla)
-
+function J = J(x)
+    J = [
+        cos(x(1) * x(2)) * x(2) / 2 - 1/2, cos(x(1) * x(2)) * x(1) / 2 - 1 / (4 * pi);
+        (1 - 1 / (4 * pi)) * exp(2 * x(1)) * 2 - 2 * e, e / pi
+        ];
 endfunction
 
-function NR()
+tol = 1e-5;
+semillas = [linspace(-2, 2, 5); linspace(-2, 2, 5)]'
+% figure(1); clf; hold on;
+for s = 1:rows(semillas)
+    x0 = semillas(s, :)';
+    printf("Desde [%5.2f, %5.2f] ", x0)
+    x1 = x0;
+    n = 0;
+    do
+        n++;
+        x0 = x1;
+        x1 = x0 - J(x0) \ f(x0);
+        % plot([x0(1) ...])
+    until (norm(x1 - x0, "inf") / norm(x1, "inf") < tol)
+    printf("en %2d iteraciones se obtuvo [%18.14f %18.14f]\n", n, x1);
+endfor
 
-endfunction
+% plot([x1(1 semillas(s, 1))], [x1(2) semillas(s, 2)], );
+% plot(x1(1), x1(2));
+
+disp("FIN DE EJERCICIO")
