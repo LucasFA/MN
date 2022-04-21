0f25416377878830e1139321bc4c025214b085b5
LucasFA <lucaslanzarote@gmail.com>
2022-04-21 22:02:15 +0200

    Examen terminado

---
+++ Practicas/Parc1PracB.m
@@ -0,0 +1,54 @@
+clc; clear all; more off; hold off;
+
+% Ec de iteracion funcional
+function y = g(x)
+    y = (exp(-x) + 1 - 8 * x) ./ (2 * (sin(x) + 2));
+endfunction
+
+t = linspace(-1, 1, 20);
+plot(t, t, 'b') # azul
+hold on;
+plot(t, g(t), 'r') # rojo
+plot(t, 0, 'k')
+hold off;
+
+printf("Observamos que g tiene una pendiente negativa muy baja: podemos asegurar que |g(x)| < 1 para todo x en un entorno ")
+printf("que incluye tanto a la raiz de f como a la semilla x0 = 0.25.\n")
+disp("Por tanto, podemos asegurar la convergencia del metodo de iteracion funcional, recordando el teorema de convergencia global")
+
+% -----------------------------------------------
+# Apartado 2
+printf("\n")
+printf("Tabla de iteraciones de Steffensen:\n");
+printf("Iteracion   aproximacion   diferencia absoluta\n");
+
+formato = "%18.8f";
+formatoFinal = ["  %2d " formato formato "\n"];
+
+tol = 1e-4;
+err = 0;
+x = [0 0 0];
+
+xanterior = 0.25;
+iteraciones = 0;
+
+printf(formatoFinal, iteraciones, xanterior, err);
+do
+
+    x(1) = xanterior;
+    for i = 2:3
+        x(i) = g(x(i - 1));
+    endfor
+
+    # aplicamos Aitken al tercer paso:
+    % Si d es la diferencia progresiva, asumida lineal, d(d(x)) == d(x(3) - x(2)) ==
+    % == x(3) - x(2) - x(2) + x(1) == x(3) - 2*x(2) + x(1)
+    xnueva = x(3) - (x(3) - x(2))^2 / (x(3) - 2 * x(2) + x(1));
+    iteraciones++;
+    err = abs(xnueva - xanterior);
+
+    printf(formatoFinal, iteraciones, xnueva, err)
+
+    xanterior = xnueva;
+until (err < tol || iteraciones > 1000)
+% El criterio de parada por iteraciones no esta pedido, pero es una medida de seguridad
