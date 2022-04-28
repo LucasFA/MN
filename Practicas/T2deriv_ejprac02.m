8b15975e3d392d342e28837db8cdc5c6d2770ca0
LucasFA <lucaslanzarote@gmail.com>
2022-04-28 20:49:51 +0200

    Terminado segundo apartado del ej02

--- Practicas/T2deriv_ejprac02.m
+++ Practicas/T2deriv_ejprac02.m
@@ -2,7 +2,7 @@ clc; clear all; more off;
 
 disp("Ejercicio 2. Derivacion numerica")
 
-a = .1; k = 2; nodos = -2:1;
+a = -1; k = 2; nodos = -2:1;
 n = length(nodos) - 1;
 printf("\nFormula para el calculo de la derivada\n")
 printf("de f de orden k = %d en el punto a = %f\n", k, a)
@@ -26,6 +26,38 @@ disp("Matriz de Vandermonde y de terminos independientes:")
 disp([M b])
 
 alpha = M\b;
+
+printf("Pesos:\n")
+disp(alpha);
+# CHecked output for a): it's alright
+
+% apartado b
+clear all;
+a = 1;
+k = 1;
+nodos = [-1 -0.5 0 1];
+
+n = length(nodos) - 1
+printf("\nFormula para el calculo de la derivada\n")
+printf("de f de orden k = %d en el punto a = %f\n", k, a)
+disp("con nodos")
+disp(nodos);
+
+M = ones(n + 1, n + 1);
+for i = 1:n
+    M(i + 1, :) = nodos.^i;
+endfor
+
+b = zeros(n + 1, 1);
+
+for i = k:n
+    b(i + 1, 1) = factorial(i) * a^(i - k) / factorial(i - k);
+endfor
+
+disp("Matriz de Vandermonde y de terminos independientes:")
+disp([M b])
+
+alpha = M \ b;
 
 printf("Pesos:\n")
 disp(alpha);
