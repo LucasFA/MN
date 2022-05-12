89e9055b3ccb1914e614847846f71b44d73247c1
LucasFA <lucaslanzarote@gmail.com>
2022-05-12 20:59:35 +0200

    Terminado... or is it? (╯°□°）╯︵ ┻━┻

---
+++ Tema 2/T2integr_ejprac03.m
@@ -0,0 +1,57 @@
+clc; clear all;
+disp("EJERCICIO 3")
+TOL = 1e-4;
+function y = f(x)
+    y = x .* exp(-x / 3);
+endfunction
+
+a = 0; b = 3;
+exacto = quad(@f, a, b)
+
+disp(" Formula  n  eval  aprox error")
+disp("-------------------------------")
+formato = "%11s    %2d    %3d    %14.8f    %10.2e\n"
+
+# ------------------------------------------
+
+disp("Newton-Cotes:")
+% NC
+
+n = 0;
+do
+    n++; h = (b - a) / 2; s = 0;
+    for i = 1:n
+        [nodos pesos] = integralNC(a + (i - 1) * h, a + i * h, 4, false, false);
+        s = s + pesos * f(nodos)';
+    endfor
+until abs(s - exacto) < TOL;
+
+printf(formato, " NewtonCotes", n, n * 3, s, abs(s - exacto))
+
+# ------------------------------------------
+% Simpson
+n = 1;
+do
+    n++; h = (b - a) / n;
+    s4 = sum(f(linspace(a + h / 2, b - h / 2, n))); # nodos de en medio (ponderados por 4/6)
+    s2 = sum(f(linspace(a + h, b - h, n - 1))); # nodos de enlace
+
+    s = h / 6 * (f(a) + 4 * s4 + 2 * s2 +f(b));
+
+until abs(s - exacto) < TOL;
+
+printf(formato, "simpson", n, 2 * n + 1, s, abs(s - exacto))
+# ------------------------------------------
+% trapecio
+n = 1;
+do
+    n++; h = (b - a) / n;
+    s = h / 2 * (f(a) + 2 * sum(f(linspace(a + h, b - h, n - 1))) + f(b));
+until abs(s - exacto) < TOL;
+
+printf(formato, "trapecio", n, n + 1, s, abs(s - exacto))
+disp("===================================================")
+disp("FINAL DEL EJERCICIO")
+disp("Observamos que trapecio no es eficiente")
+disp("Newton Cotes tiene bueb rendimiento, aunque es algo complicada")
+disp("Simpson es mas sencilla y es muy eficiente")
---
+++ Tema 2/T2integr_ejprac04.m
@@ -0,0 +1,32 @@
+clc; clear all;
+disp("EJERCICIO 4")
+
+TOL = 1e-4;
+function y = f(x)
+    y = x .* exp(-x / 3);
+endfunction
+
+disp("  N   iter   aprox   criterio")
+a = 0; b = 3;
+exacto = quad(@f, a, b)
+
+function N = tablaRomberg(a, b, MaxIter, tol)
+    formato = "   %d  %3d  %12.8f\n";
+    N = 1; n = 1; h = b - a;
+    R(1, 1) = h / 2 * (f(a) + f(b));
+    printf(formato, 0, 1, R(1, 1));
+    do
+        x = linspace(a + h / 2, b - h / 2, n);
+        R(N + 1, 1) = 1/2 * (R(N, 1) + h * sum(f(x)));
+        N++;
+        n = 2 * n;
+        h = h / 2;
+        for k = 2:N
+            coef = 4^(k-1);
+            R(N, k) = (coef*R(N,k-1) - R(N-1, k - 1)) / (coef-1);
+        endfor
+        printf(formato, N, 2^N, R(N,N), abs(R(N-1,N-1)))
+    until abs(R(N-1,N-1) - R(N,N)) < tol || N > MaxIter;
+endfunction
+
+tablaRomberg(a, b, 100, 1e-5, TOL)
