a1d2f69850e71b06af73271cd3aaffed0614aa68
LucasFA <lucaslanzarote@gmail.com>
2022-05-13 00:03:21 +0200

    Lint, add missing entry in output

--- Tema 2/T2integr_ejprac04.m
+++ Tema 2/T2integr_ejprac04.m
@@ -6,15 +6,15 @@ function y = f(x)
     y = x .* exp(-x / 3);
 endfunction
 
-disp("  N   iter   aprox   criterio")
+disp("    N  iter      aprox       criterio")
 a = 0; b = 3;
-exacto = quad(@f, a, b)
+exacto = quad(@f, a, b);
 
 function N = tablaRomberg(a, b, MaxIter, tol)
-    formato = "   %d  %3d  %12.8f\n";
+    formato = "   %d  %3d  %12.8f  %12.8f\n";
     N = 1; n = 1; h = b - a;
     R(1, 1) = h / 2 * (f(a) + f(b));
-    printf(formato, 0, 1, R(1, 1));
+    printf(formato, 0, 1, R(1, 1), 0);
     do
         x = linspace(a + h / 2, b - h / 2, n);
         R(N + 1, 1) = 1/2 * (R(N, 1) + h * sum(f(x)));
@@ -22,11 +22,11 @@ function N = tablaRomberg(a, b, MaxIter, tol)
         n = 2 * n;
         h = h / 2;
         for k = 2:N
-            coef = 4^(k-1);
-            R(N, k) = (coef*R(N,k-1) - R(N-1, k - 1)) / (coef-1);
+            coef = 4^(k - 1);
+            R(N, k) = (coef * R(N, k - 1) - R(N - 1, k - 1)) / (coef - 1);
         endfor
-        printf(formato, N, 2^N, R(N,N), abs(R(N-1,N-1)))
-    until abs(R(N-1,N-1) - R(N,N)) < tol || N > MaxIter;
+        printf(formato, N, 2^N, R(N, N), abs(R(N - 1, N - 1) - R(N, N)))
+    until abs(R(N - 1, N - 1) - R(N, N)) < tol || N > MaxIter;
 endfunction
 
-tablaRomberg(a, b, 100, 1e-5, TOL)
+tablaRomberg(a, b, 100, TOL);
