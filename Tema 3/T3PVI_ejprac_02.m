d767ad267083ff60199143ab7f80e2f6b2f827b0
Lucas <lucaslanzarote@gmail.com>
2022-05-25 20:32:36 +0200

    Update T3PVI_ejprac_02.m

--- Tema 3/T3PVI_ejprac_02.m
+++ Tema 3/T3PVI_ejprac_02.m
@@ -1,3 +1,5 @@
+#   TODO: eliminar cálculos redundantes de s.
+
 clc; 
 function y = f(t,x)
   y = x-t^2+1;
@@ -17,16 +19,30 @@ function x = s(t)
 endfunction
 
 
-t = a; x = mu; N = 080; h = (b-a)/N;  # 0.025 para Euler
+t = a; x = mu; N = 80; h = (b-a)/N;  # 0.025 para Euler
 
 for n = 1:N
   x = [x Euler(t(end), x(end), h)];
   t = [t t(end)+h];
 endfor
+
 tEu = t; xEu = x; sEu = s(t);
+
+# Por qué cambiamos el paso? Para que el número de cáculos de f sea el mismo
+# para los dos métodos
 
 t = a; x = mu; N = 40; h = (b-a)/N; # 0.05 para PtoMedio
 for i= 1:N
   x = [x PtoMedio(t(end), x(end), h];  
 endfor
 
+tPm = t; xPm = x; sPm = s(t);
+
+x =linspace(a,b,60);
+figure(1);
+
+plot(tEu, xEu, "b;Euler;" tPm, xPm, "r;PtoMedio;" x,s(x), "k;Exacta");
+plot(tEu, xEu - s(tEu), "b;Euler;" tPm, xPm - s(xPm), "r;PtoMedio;");
+
+disp("De la segunda gráfica se deduce que el punto medio es más exacta");
+disp("========================FIN========================")
