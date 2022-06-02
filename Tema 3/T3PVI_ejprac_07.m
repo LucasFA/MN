f4786386e1e81788e374933bfc568e89de06e3c4
LucasFA <lucaslanzarote@gmail.com>
2022-06-02 20:53:14 +0200

    Intento de práctica terminada

---
+++ Tema 3/T3PVI_ejprac_07.m
@@ -0,0 +1,29 @@
+clc; clear all; more off;
+
+function y = f(t, x)
+    y = 0.05 * x * (1 - log(x));
+endfunction
+
+ah = [20 0.25];
+
+a = 0; b = 100; mu = 1;
+
+% vers con Butchersemi
+Ba = [0 0; 2/3 0];
+Bb = [1/4 3/4];
+Bc = [0 2/3];
+
+for h = ah
+    N = round((b - a) / h);
+    t = a * ones(1, N + 1);
+    x = mu * ones(1, N + 1);
+
+    for n = 1:N
+        [t(n + 1) x(n + 1)] = ButcherSemi(Ba, Bb, Bc, t(n), x(n), h);
+    endfor
+
+    plot(t, x, [";h;=" num2str(h) ";"]);
+endfor
+
+disp("======================")
+
