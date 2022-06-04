1a0f8610dfe4d6addd771fee9112d26b94d745a9
LucasFA <lucaslanzarote@gmail.com>
2022-06-04 17:49:08 +0200

    Typo, ahora superpone gráficas

--- Tema 3/T3PVI_ejprac_06.m
+++ Tema 3/T3PVI_ejprac_06.m
@@ -1,11 +1,12 @@
 clc; clear all; more off;
-disp("Ejercicio 5")
 function y = f(t, x, x1)
     y = fsolve(@(u) u^2 + t * x - exp(-u) - t, x1);
 endfunction
 
-a = 0; b = 10; mu = 0; h = 0.1;
+a = 0; b = 10; mu = 0; 
+h = 0.1;
 
+disp("Ejercicio 5")
 x1 = f(a, mu, 0);
 
 N = round((b - a) / h);
@@ -18,8 +19,9 @@ for n = 1:N
     x1 = (x(n + 1) - x(n)) / h; # proxima semilla
 endfor
 
+figure(1); clf; 
+plot(t, x, ";xn;");
 hold on
-figure(1); clf; plot(t, x, ";xn;");
 disp("==== FIN ================================================")
 
 clc; clear all;
@@ -41,7 +43,7 @@ x = mu * ones(1, N + 1);
 for n = 1:N
     fn = f(t(n), x(n), x1);
     x(n + 1) = fsolve(@(w) x(n) + h / 2 * (fn + f(t(n) + h, w, x1)) - w, x(n));
-    t(n + 1) = t(n) + 1;
+    t(n + 1) = t(n) + h;
     x1 = (x(n + 1) - x(n)) / h;
 endfor
 
